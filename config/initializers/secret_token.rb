feb2b5c47dc386bd91674febac7dac4219f0990a
Steve Yeom <krazyeom@gmail.com>
2013-08-30 13:02:32 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weirdmeetup::Application.config.secret_token = '9c7a35e1f5ff2301417238c819a0fde4b0911a6a21f4d2025f6eafec3ab44ad538ba9727a5b9343c23e60658fc8f3f739cb61d2747b651b042e0a765f7e1dd02'
